2d5a13f8ad1d4c5c44510f037808bfb25a6603b6
Marie Cassandre <mariecassandrea2005@gmail.com>
2026-03-05 10:13:28 +0000

    correction bug

--- src/calculator.js
+++ src/calculator.js
@@ -27,6 +27,6 @@ function modulo(a, b) {
 
 module.exports = { add, subtract, multiply, divide, modulo };
 
-function add(a, b) {
-  return a - b; 
-}
+// function add(a, b) {
+//   return a - b; 
+// }
